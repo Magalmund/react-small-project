b9706ec59c556a224663f0a7fa44421672ac0281
Kaloid <poker6-96@mail.ru>
2022-09-02 00:35:19 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import {Success} from './components/Success';
+import {Users} from './components/Users';
 
 // Тут список пользователей: https://reqres.in/api/users
 
-function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+function App () {
+    const [users, setUsers] = useState([])
+    useEffect(() => {
+        fetch(https://reqres.in/api/users)
+    }, [])
+    return (
+        <div className="App">
+            <Users/>
+            {/* <Success /> */}
+        </div>
+    );
 }
 
 export default App;
