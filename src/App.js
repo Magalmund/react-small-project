9c693edb2eb83ab1d47b7d73e2c07306f39fa5b2
Kaloid <poker6-96@mail.ru>
2022-09-02 00:53:28 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.scss';
 import {Success} from './components/Success';
 import {Users} from './components/Users';
@@ -8,7 +8,12 @@ import {Users} from './components/Users';
 function App () {
     const [users, setUsers] = useState([])
     useEffect(() => {
-        fetch(https://reqres.in/api/users)
+        fetch('https://reqres.in/api/users').then(res => res.json()).then(json => {
+            setUsers(json.data)
+        }).catch(err => {
+            console.warn(err)
+            alert('Ошибка при получении пользователей')
+        })
     }, [])
     return (
         <div className="App">
