ab6ac7e8c612c9770b748cfe786a2872791fb636
Kaloid <poker6-96@mail.ru>
2022-09-03 23:02:51 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -7,18 +7,51 @@ import {Users} from './components/Users';
 
 function App () {
     const [users, setUsers] = useState([])
+    const [isLoading, setLoading] = useState(true)
+    const [invites, setInvites] = useState([])
+    const [success, setSuccess] = useState(false)
+    const [searchValue, setSearchValue] = useState('')
     useEffect(() => {
         fetch('https://reqres.in/api/users').then(res => res.json()).then(json => {
             setUsers(json.data)
         }).catch(err => {
             console.warn(err)
             alert('Ошибка при получении пользователей')
-        })
+        }).finally(() => setLoading(false))
     }, [])
+
+    const onChangeSearchValue = (event) => {
+        setSearchValue(event.target.value)
+    }
+
+    const onClickInvite = (id) => {
+        if(invites.includes(id)) {
+            setInvites(prev => prev.filter(_id => _id !== id));
+        } else {
+            setInvites((prev) => [...prev, id])
+        }
+    }
+
+    const onClickSendInvites = () => {
+        setSuccess(true)
+    }
+
     return (
         <div className="App">
-            <Users/>
-            {/* <Success /> */}
+            {
+                success ? (<Success count={invites.length}/>) : (
+                    <Users
+                        onChangeSearchValue={onChangeSearchValue}
+                        searchValue={searchValue}
+                        items={users}
+                        isLoading={isLoading}
+                        invites={invites}
+                        onClickInvite={onClickInvite}
+                        onClickSendInvites={onClickSendInvites}
+                    />
+                )
+            }
+
         </div>
     );
 }
